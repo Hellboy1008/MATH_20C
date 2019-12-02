e7ed357e3a2d5f5d19d0d091d2bd257d57bacbf2
Stephen Lee <srnlee771@gmail.com>
2019-12-01 18:20:50 -0800

    update date edited

--- MATH20C_Chapter_One.java
+++ MATH20C_Chapter_One.java
@@ -2,7 +2,7 @@
 /**
  * Created by: 龍ONE 
  * Date Created: December 19, 2018
- * Date Edited: November 30, 2019
+ * Date Edited: December 1, 2019
  * Purpose: Perform Calculations for MATH20C Problems in Chapter 1 of the textbook.
  */
 
