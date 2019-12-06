d873997c8a1d73d3aa81ce75df10aecd6f85c322
Stephen Lee <srnlee771@gmail.com>
2019-12-05 17:21:40 -0800

    change error message

--- MATH20C_Calculations.java
+++ MATH20C_Calculations.java
@@ -19,13 +19,13 @@ public class MATH20C_Calculations {
     private static final char LEFT_PARENTHESIS = '(';
     private static final char RIGHT_PARENTHESIS = ')';
 
-    private static final String LINES_NOT_IN_R3 = "Error: The line/lines are not in R3";
-    private static final String NOT_SQUARE_MATRIX = "Error: The matrix is not a square matrix";
-    private static final String PLANES_NOT_IN_R3 = "Error: The plane/planes are not in R3";
-    private static final String POINTS_NOT_IN_R3 = "Error: The point/points are not in R3";
-    private static final String POINTS_NOT_IN_SAME_DIMENSION = "Error: The points are not in the same dimension";
-    private static final String VECTORS_NOT_IN_R3 = "Error: The vector/vectors are not in R3";
-    private static final String VECTORS_NOT_IN_SAME_DIMENSION = "Error: The vectors are not in the same dimension";
+    private static final String LINES_NOT_IN_R3 = "Incorrect input -> The line/lines are not in R3";
+    private static final String NOT_SQUARE_MATRIX = "Incorrect input -> The matrix is not a square matrix";
+    private static final String PLANES_NOT_IN_R3 = "Incorrect input -> The plane/planes are not in R3";
+    private static final String POINTS_NOT_IN_R3 = "Incorrect input -> The point/points are not in R3";
+    private static final String POINTS_NOT_IN_SAME_DIMENSION = "Incorrect input -> The points are not in the same dimension";
+    private static final String VECTORS_NOT_IN_R3 = "Incorrect input -> The vector/vectors are not in R3";
+    private static final String VECTORS_NOT_IN_SAME_DIMENSION = "Incorrect input -> The vectors are not in the same dimension";
 
     /**
      * This method converts a string of numbers into a double array.
